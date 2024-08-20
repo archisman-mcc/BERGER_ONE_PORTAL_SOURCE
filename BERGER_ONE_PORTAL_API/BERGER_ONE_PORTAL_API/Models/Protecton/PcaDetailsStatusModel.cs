5c815729b113696e23bd92ff3618399613a16ef6
subhrajit <subhrajit.mukherjee@mccit.co.in>
2024-08-20 11:29:51 +0530

    Epca APIs added

---
+++ BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Models/Protecton/PcaDetailsStatusModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BERGER_ONE_PORTAL_API.Models.Protecton
+{
+    public class PcaDetailsStatusModel
+    {
+        [JsonProperty("pd_auto_id")]
+        public Decimal? pd_auto_id { get; set; }
+
+        [JsonProperty("pd_status")]
+        public string? pd_status { get; set; }
+    }
+}
--- BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Repository/Protecton/IEpcaRepo.cs
+++ BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Repository/Protecton/IEpcaRepo.cs
@@ -12,5 +12,10 @@ namespace BERGER_ONE_PORTAL_API.Repository.Protecton
         Task<MSSQLResponse?> GetSKUList(GetSKUListRequestDto dto);
         Task<MSSQLResponse?> GetPcaBillToList(GetBillToRequestDto dto);
         Task<MSSQLResponse?> GetFactoryListBySKU(GetFactoryRequestDto dto, string User_id);
+        Task<MSSQLResponse?> GetPcaMinRateBySku_Vr1(GetMinRateBySkuRequestDto dto);
+        Task<MSSQLResponse> InsertePcaDetails_Vr1(PcaInsertRequestDto request, string User_id);
+        Task<MSSQLResponse?> PcaDetailsGetStatus(GetPcaDetailsStatusRequestDto dto);
+        Task<MSSQLResponse?> PcaDetailsGetDtl(GetPcaDetailsRequestDto dto, string User_id);
+        Task<MSSQLResponse> DeletePcaDetails(DeletePCARequestDto request, string User_id);
     }
 }
