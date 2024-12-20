e0867d698975c3888e4b296172eb2b1ebb395a43
soumyashubhraMCC <soumyashubhra.roy@mccit.co.in>
2024-12-20 13:44:53 +0530

    Excel Report API

--- BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Repository/Utility/Utlis.cs
+++ BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Repository/Utility/Utlis.cs
@@ -10,5 +10,29 @@ namespace BERGER_ONE_PORTAL_API.Repository.Utility
             var serializeObject = JsonConvert.SerializeObject(dt);
             return JsonConvert.DeserializeObject<List<T>>(serializeObject) ?? [];
         }
+
+        public static bool HasData(this DataSet? ds, int tableCount)
+        {
+            bool result = false;
+
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                for (int i = 0; i < tableCount; i++)
+                {
+                    if (ds.Tables[i] != null && ds.Tables[i].Rows.Count > 0)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+
+        }
     }
 }
