99abe5fc78cf7bd130d06687fe52e04f25454d2d
Sanju Adak <sanju.adak@mccit.co.in>
2025-05-02 18:57:53 +0530

    Multilevel menu API added in validate login Vr1

--- BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Logic/ILoginLogic.cs
+++ BERGER_ONE_PORTAL_API/BERGER_ONE_PORTAL_API/Logic/ILoginLogic.cs
@@ -7,5 +7,8 @@ namespace BERGER_ONE_PORTAL_API.Logic
         Task<LoginResponseDto?> ValidateLogin(LoginRequestDto? request);
         Task<LoginResponseDto?> ValidateRefreshToken(string user_id, string refresh_token);
         Task<LoginResponseDto?> ValidateRefreshTokenV1(TokenRefreshDtoNew? tokenRefreshNewDto);
-    }
+        Task<LoginResponseDto?> ValidateLoginVr1(LoginRequestDto? request);
+		Task<LoginResponseDto?> ValidateRefreshTokenV2(TokenRefreshDtoNew? tokenRefreshNewDto);
+
+	}
 }
