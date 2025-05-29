9359c6d2f422bc107e669ea8bc3325ea5509da70
Sayan Pandab <sayan.pandab@mccit.co.in>
2025-05-29 14:32:41 +0530

    changes_14:33

--- berger-one/src/App.tsx
+++ berger-one/src/App.tsx
@@ -3,14 +3,11 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import './styles/custom.css';
-// import './styles/tailwind.css';
-import './App.css'
+// import './App.css';
 import Login from './pages/login/Login'
 import Dashboard from './pages/dashboard/Dashboard';
-// import { UseAuthStore } from './services/store/AuthStore';
 import FixedLayout from './pages/fixedLayout/FixedLayout';
 import EPCAList from './pages/protecton/ePCA/EPCAList';
-import { UseAuthStore } from './services/store/AuthStore';
 import EPCADetails from './pages/protecton/ePCA/EPCADetails';
 import EPCADepotApprovalList from './pages/protecton/ePCA/EPCADepotApprovalList';
 import EPCADepotApprovalDetails from './pages/protecton/ePCA/EPCADepotApprovalDetails';
@@ -26,80 +23,80 @@ import TLVRevisionRSMApproval from './pages/protecton/TLV/TLVRevisionRSMApproval
 import TLVRevisionHoApproval from './pages/protecton/TLV/TLVRevisionHoApproval';
 
 function App() {
-  const user = UseAuthStore((state: any) => state.userDetails);
-  // console.log(user?.user_id)
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <FixedLayout />,
-      children: [
-        {
-          path: '/',
-          element: <Dashboard />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCAList',
-          element: <EPCAList />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCADetails',
-          element: <EPCADetails />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCADepotApprovalList',
-          element: <EPCADepotApprovalList />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCADepotApprovalDetails',
-          element: <EPCADepotApprovalDetails />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCARsmApprovalList',
-          element: <EPCARsmApprovalList />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCARsmApprovalDetails',
-          element: <EPCARsmApprovalDetails />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCAHOApprovalList',
-          element: <EPCAHOApprovalList />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCAHoApprovalDetails',
-          element: <EPCAHoApprovalDetails />,
-        },
-        {
-          path: '/Protecton/ePCA/EPCACancellation',
-          element: <EPCACancellation />,
-        },
-        {
-          path: '/Protecton/TLV/TLVRevisionRequestList',
-          element: <TLVRevisionRequestList />,
-        },
-        {
-          path: '/Protecton/TLV/TLVRevisionRequestDetails',
-          element: <TLVRevisionRequestDetails />,
-        },
-        {
-          path: '/Protecton/TLV/TLVRevisionDepotApproval',
-          element: <TLVRevisionDepotApproval />,
-        },
-        {
-          path: '/Protecton/TLV/TLVRevisionRSMApproval',
-          element: <TLVRevisionRSMApproval />,
-        },
-        {
-          path: '/Protecton/TLV/TLVRevisionHoApproval',
-          element: <TLVRevisionHoApproval />,
-        },
-      ],
-    },
-    {
-      path: '/login/cover-login/',
-      element: <Login />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <FixedLayout />,
+        children: [
+          {
+            path: '/',
+            element: <Dashboard />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCAList',
+            element: <EPCAList />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCADetails',
+            element: <EPCADetails />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCADepotApprovalList',
+            element: <EPCADepotApprovalList />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCADepotApprovalDetails',
+            element: <EPCADepotApprovalDetails />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCARsmApprovalList',
+            element: <EPCARsmApprovalList />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCARsmApprovalDetails',
+            element: <EPCARsmApprovalDetails />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCAHOApprovalList',
+            element: <EPCAHOApprovalList />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCAHoApprovalDetails',
+            element: <EPCAHoApprovalDetails />,
+          },
+          {
+            path: '/Protecton/ePCA/EPCACancellation',
+            element: <EPCACancellation />,
+          },
+          {
+            path: '/Protecton/TLV/TLVRevisionRequestList',
+            element: <TLVRevisionRequestList />,
+          },
+          {
+            path: '/Protecton/TLV/TLVRevisionRequestDetails',
+            element: <TLVRevisionRequestDetails />,
+          },
+          {
+            path: '/Protecton/TLV/TLVRevisionDepotApproval',
+            element: <TLVRevisionDepotApproval />,
+          },
+          {
+            path: '/Protecton/TLV/TLVRevisionRSMApproval',
+            element: <TLVRevisionRSMApproval />,
+          },
+          {
+            path: '/Protecton/TLV/TLVRevisionHoApproval',
+            element: <TLVRevisionHoApproval />,
+          },
+        ],
+      },
+      {
+        path: '/login/cover-login/',
+        element: <Login />,
+      },
+    ]
+  );
 
   return (
     <div className="min-h-screen bg-blue-300 text-black">
