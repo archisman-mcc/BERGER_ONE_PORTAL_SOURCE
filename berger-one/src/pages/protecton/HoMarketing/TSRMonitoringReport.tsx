a0310ecfe9428d78d73eb4229ab88c6853757230
Sayan Pandab <sayan.pandab@mccit.co.in>
2026-01-06 12:27:47 +0530

    TSRMonitoringReport_ch

--- berger-one/src/pages/protecton/HoMarketing/TSRMonitoringReport.tsx
+++ berger-one/src/pages/protecton/HoMarketing/TSRMonitoringReport.tsx
@@ -50,7 +50,8 @@ const TSRMonitoringReport = () => {
         { value: '2022', label: '2022' },
         { value: '2023', label: '2023' },
         { value: '2024', label: '2024' },
-        { value: '2025', label: '2025' }
+        { value: '2025', label: '2025' },
+        { value: '2026', label: '2026' }
     ];
 
     const GetRegion = async () => {
